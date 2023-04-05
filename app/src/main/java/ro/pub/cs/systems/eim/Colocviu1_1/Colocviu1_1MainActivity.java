476f2044fab18977602ee8c31b80eb35a159c8cd
andrei.dragomir3105 <andrei.dragomir3105@stud.acs.upb.ro>
2023-04-05 09:23:36 +0300

    Added B

--- app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_1/Colocviu1_1MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_1/Colocviu1_1MainActivity.java
@@ -1,16 +1,19 @@
 package ro.pub.cs.systems.eim.Colocviu1_1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Colocviu1_1MainActivity extends AppCompatActivity {
 
     private Button northBtn, westBtn, eastBtn, southBtn;
     private String messageString = "";
-
+    private Integer noCardinal = 0;
     private ButtonClickListener buttonClickListener = new ButtonClickListener();
 
     private class ButtonClickListener implements View.OnClickListener {
@@ -22,22 +25,33 @@ public class Colocviu1_1MainActivity extends AppCompatActivity {
             else {
                 messageString.concat(",").concat(text);
             }
-
+            TextView noCardinal = (TextView) findViewById(R.id.noCardinalTextView);
+            noCardinal.setText((CharSequence) noCardinal.toString());
         }
+
         @Override
         public void onClick(View view) {
             switch (view.getId()) {
                 case R.id.north_button:
+                    noCardinal++;
                     addToString(northBtn.getText().toString());
+                    noCardinal++;
                     break;
                 case R.id.west_button:
                     addToString(westBtn.getText().toString());
+                    noCardinal++;
                     break;
                 case R.id.east_button:
                     addToString(eastBtn.getText().toString());
+                    noCardinal++;
                     break;
                 case R.id.south_button:
                     addToString(southBtn.getText().toString());
+                    noCardinal++;
+                    break;
+                case R.id.navigate_button:
+                    Intent intent = new Intent(getApplicationContext(), Colocviu1_1SecondaryActivity.class);
+                    intent.putExtra(Constants.CARDINAL, messageString);
                     break;
             }
             finish();
@@ -59,5 +73,21 @@ public class Colocviu1_1MainActivity extends AppCompatActivity {
         eastBtn.setOnClickListener(buttonClickListener);
         southBtn.setOnClickListener(buttonClickListener);
 
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(Constants.NO_CARDINAL, noCardinal);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState.containsKey(Constants.NO_CARDINAL)) {
+            noCardinal = savedInstanceState.getInt(Constants.NO_CARDINAL);
+            TextView noCardinal = (TextView) findViewById(R.id.noCardinalTextView);
+            noCardinal.setText((CharSequence) noCardinal.toString());
+        }
     }
 }
---
+++ app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_1/Constants.java
@@ -0,0 +1,6 @@
+package ro.pub.cs.systems.eim.Colocviu1_1;
+
+public class Constants {
+    public static final String NO_CARDINAL = "no_buttons";
+    public static final String CARDINAL ="cardinal";
+}
